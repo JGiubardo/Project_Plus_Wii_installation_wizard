161a4f621e0ca3e373a62a36a69e4f09d6ab525b
Joseph Giubardo <jgiubard@terpmail.umd.edu>
2023-04-09 13:23:12 -0400

    Update text description

--- extract.py
+++ extract.py
@@ -1,12 +1,13 @@
+import os
+import sys
 from tkinter import messagebox, Tk, Button
-from win32api import GetLogicalDriveStrings
-from py7zr import SevenZipFile
 from psutil import disk_usage, disk_partitions
-import sys, os
+from py7zr import SevenZipFile
+from win32api import GetLogicalDriveStrings
 
 MAX_DRIVE_SIZE = 32 * 1024 * 1024 * 1024
 if getattr(sys, 'frozen', False):
-    P_PLUS_ZIP = file=os.path.join(sys._MEIPASS, 'files\\PPlus2.3.2.7z')
+    P_PLUS_ZIP = file = os.path.join(sys._MEIPASS, 'files\\PPlus2.3.2.7z')
 else:
     P_PLUS_ZIP = 'PPlus2.3.2.7z'
 # P_PLUS_ZIP = 'test.7z'
@@ -23,7 +24,7 @@ def select_drive(ignore_problems=False):
     try:
         path = drive + "\\"
         print(path)
-    except NameError as e:
+    except NameError:
         messagebox.showerror("Error", "Drive not selected")
         exit()
     if not ignore_problems:
@@ -45,11 +46,11 @@ def drive_selector_gui():
     gui = Tk()
     gui.title("Select Drive")
     drives = [x[:2] for x in GetLogicalDriveStrings().split('\x00')[:-1]]
-    font = ('Consolas', 18, 'bold')
+    font = ('Consolas', 20, 'bold')
     for i, drv in enumerate(drives):
         Button(text=drv, font=font, width=5, command=lambda d=drv: drive_selected(gui, d)).grid(row=i // 5,
                                                                                                 column=i % 5,
-                                                                                                padx=5, pady=3)
+                                                                                                padx=8, pady=5)
     gui.mainloop()
 
 
@@ -80,6 +81,5 @@ if __name__ == '__main__':
     drive = select_drive()
     extract_to_drive(drive)
     messagebox.showinfo("Complete",
-                        "Mod extracted, place SD in console and boot through stage builder or homebrew channel. A "
-                        "Brawl disc or backup is required to play")
-
+                        "Mod extracted, place SD in console and boot through stage builder or homebrew channel. An "
+                        "NTSC Brawl disc or backup is required to play")
